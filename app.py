1e79f4bdb470926c00a2127ce27971db4550c43a
Suhana Shaik <suhanashaik2657@gmail.com>
2023-10-20 12:22:57 +0530

    updated frontend

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 conn=mysql.connector.connect(
     host='localhost',
     user='root',
-    password='shauryanoob',
+    password='tesla@2005',
     database="WEBSITE"
 )
 
