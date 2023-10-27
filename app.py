64e4444c21b709be5568e8bd3b756436a18ec2aa
Shauryanoobb <shauryak3477@gmail.com>
2023-10-27 15:33:47 +0530

    second draft

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 conn=mysql.connector.connect(
     host='localhost',
     user='root',
-    password='shauryanoob'
+    password='Shaurya3477',
     database="WEBSITE"
 )
 
